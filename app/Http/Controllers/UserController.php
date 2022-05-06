81eb8d5e4917e87715f2d950cc3f47a4c45b51cc
parlinduha <dhparlin@gmail.com>
2022-05-06 21:09:10 +0700

    make endpoint register and controller register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,4 +12,12 @@ class UserController extends Controller
     {
         return User::all();
     }
+    public function Register(Request $req)
+    {
+        $user = new User;
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->password = Hash::make($req->input('password'));
+        $user->level = 
+      }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/user', [UserController::class,'Index']);
+Route::post('/register', [UserController::class,'Register']);
